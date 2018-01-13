3d188288740aa1d81db81cbcbae7c24eed77bb65
sydney-ng <sydney.ng@yahoo.com>
2018-01-12 16:28:59 -0800

    lecture notes 1/12

---
+++ Projects/Project1/Project1-CS32/CS32-PROJECT1/CS32-PROJECT1/VoiceMail.cpp
@@ -0,0 +1,9 @@
+//
+//  VoiceMail.cpp
+//  CS32-PROJECT1
+//
+//  Created by Super on 1/10/18.
+//  Copyright © 2018 Sydney. All rights reserved.
+//
+
+#include "VoiceMail.hpp"
---
+++ Projects/Project1/Project1-CS32/CS32-PROJECT1/CS32-PROJECT1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS32-PROJECT1
+//
+//  Created by Super on 1/10/18.
+//  Copyright © 2018 Sydney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
