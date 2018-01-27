b1a30b42a0f9441643ee9a7abbc7c738df4fca69
sydney-ng <sydney.ng@yahoo.com>
2018-01-27 13:42:00 -0800

    wrote test cases for GET2 w/ assert statements

--- Project2/CS32_Project3/CS32_Project3/main.cpp
+++ Project2/CS32_Project3/CS32_Project3/main.cpp
@@ -29,7 +29,8 @@ int main ()
     //testInsert(A);
     //testUpdate(A);
     //testInsertOrUpdate ( A);
-    testGet1(A);
+    //testGet1(A);
+    testGet2(A);
     //inserting values into the list
     /*A.insert(Key1, 1.2);
     A.insert(Key2, 3.4);
@@ -51,6 +52,82 @@ int main ()
     //Map B = A;
 }
 
+void testGet2 (Map A)
+{
+    // If 0 <= i < size(), copy into the key and value parameters the
+    // key and value of one of the key/value pairs in the map and return
+    // true.  Otherwise, leave the key and value parameters unchanged and
+    // return false.
+    
+    KeyType Key1 = "one";
+    KeyType Key2 = "two";
+    KeyType Key3 = "three";
+    KeyType Key4 = "four";
+    KeyType Key5 = "five";
+    
+    bool a = A.insertOrUpdate(Key1, 1.2);
+    bool b = A.insertOrUpdate(Key2, 2.3);
+    bool c = A.insertOrUpdate(Key3, 3.4);
+    bool d = A.insertOrUpdate(Key4, 4.5);
+    bool e = A.insertOrUpdate(Key5, 5.6);
+    assert (a == true);
+    assert (b == true);
+    assert (c == true);
+    assert (d == true);
+    assert (e == true);
+    
+    int i = -1;
+    for (i; i < A.size() +1; i++)
+    {
+        KeyType tempKey = "default";
+        ValueType tempVal = 10.1;
+        bool x = A.get(i, tempKey, tempVal);
+        if (i == -1)
+        {
+            assert (x == false);
+            assert (tempKey == "default" && tempVal == 10.1);
+        }
+        if (i == 0)
+        {
+            assert (x == true);
+            assert (tempKey == "one" && tempVal == 1.2);
+        }
+        else if (i == 1)
+        {
+            assert (x == true);
+            assert (tempKey == Key2 && tempVal == 2.3);
+        }
+        else if (i == 2)
+        {
+            assert (x == true);
+            assert (tempKey == "three" && tempVal == 3.4);
+        }
+        else if (i == 3)
+        {
+            assert (x == true);
+            assert (tempKey == Key4 && tempVal == 4.5);
+        }
+        else if (i == 4)
+        {
+            assert (x == true);
+            assert (tempKey == Key5 && tempVal == 5.6);
+        }
+        else if (i == 5)
+        {
+            assert (x == false);
+            assert (tempKey == "default" && tempVal == 10.1);
+        }
+                    
+        else if (i == 6)
+        {
+            assert (x == false);
+            assert (tempKey == "default" && tempVal == 10.1);
+        }
+    }
+    cerr << "Get2() --> passed" << endl;
+
+
+}
 void testGet1(Map A)
 {
     // If key is equal to a key currently in the map, set value to the
@@ -78,10 +155,9 @@ void testGet1(Map A)
     assert (A.get(Key2, val) == false && Key2 == "b" && val == 1.2);
     ValueType val2 = 0;
     assert (A.get(Key2, val2) == false && Key2 == "b" && val2 == 0);
+    cerr << "Get1() --> passed" << endl;
 
-   
 }
-
 
 void testInsertOrUpdate (Map A)
 {
@@ -294,8 +370,6 @@ void test_SizeEmptyContains(Map A)
     cerr << "empty() --> passed" << endl;
     cerr << "size() --> passed" << endl;
     cerr << "contains() --> passed" << endl;
-
-
     
     //after deleting 1 element
 
@@ -367,39 +441,6 @@ void testCombine (Map A, Map B)
     //result.dump();
     combine(A, B, result);
     result.dump();
-}
-
-void testGet2(Map A)
-{
-    KeyType Key1;
-    KeyType Key2;
-    KeyType Key3;
-    ValueType val;
-
-    bool z = A.get(1, Key1, val);
-    cerr << "z is : " << z << endl;
-    cout << "key is: " << Key1 << endl;
-    cout << "val is: " << val << endl<< endl;
-    
-    
-    bool a =A.get(2, Key2, val);
-    cerr << "y is : " << a<< endl;
-    cout << "key is: " << Key2 << endl;
-    cout << "val is: " << val << endl<< endl;
-    
-    a =A.get(2, Key2, val);
-    cerr << "y is : " << a<< endl;
-    cout << "key is: " << Key2 << endl;
-    cout << "val is: " << val << endl<< endl;
-    
-    cout << "-----" << endl;
-    
-    cout << A.size(); 
-    bool x= A.get(0, Key3, val);
-    cerr << "x is : " << x<< endl;
-    cout << "key is: " << Key3 << endl;
-    cout << "val is: " << val << endl;
-
 }
 
 void testSwap (Map A, Map B)
