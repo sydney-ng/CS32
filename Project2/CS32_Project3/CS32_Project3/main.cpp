0a72b5ae62cfdde828f7b2c8aeae6a135e42c6ed
sydney-ng <sydney.ng@yahoo.com>
2018-01-27 12:58:46 -0800

    added test function for update

--- Project2/CS32_Project3/CS32_Project3/main.cpp
+++ Project2/CS32_Project3/CS32_Project3/main.cpp
@@ -19,14 +19,14 @@ void testErase (Map A);
 void testSwap (Map A, Map B);
 void testCombine (Map A, Map B);
 void test_SizeEmptyContains(Map A);
-
+void testUpdate(Map A);
+void testInsertOrUpdate (Map A);
 int main ()
 {
     Map A;
-    test_SizeEmptyContains(A);
-    //testEmpty(A);
-    testInsert(A);
-    
+    //test_SizeEmptyContains(A);
+    //testInsert(A);
+    //testUpdate(A);
     //inserting values into the list
     /*A.insert(Key1, 1.2);
     A.insert(Key2, 3.4);
@@ -47,6 +47,95 @@ int main ()
     //A.dump();
     //Map B = A;
 }
+
+void testInsertOrUpdate (Map A)
+{
+    KeyType Key1 = "a";
+    KeyType Key2 = "b";
+    KeyType Key3 = "a";
+
+    
+    ValueType val = 0;
+    //updating an empty map, will not work
+    A.insertOrUpdate(Key1, 1.2);
+    assert (A.size()==0);
+    assert (A.empty()==true);
+    assert (x == false);
+    assert (A.contains(Key1) == false);
+    assert (A.get(Key1, val) == false && Key1 == "a" && val == 0);
+    
+    
+    //add an item
+    bool z = A.insert(Key1,1.2);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (z == true);
+    assert (A.contains(Key1) == true);
+    assert (A.get(Key1, val) == true && Key1 == "a" && val == 1.2);
+    
+    //update it, exiting key w/ new value
+    bool y = A.update (Key1, 1.3);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (y == true);
+    assert (A.contains(Key1) == true);
+    assert (A.get(Key1, val) == true && Key1 == "a" && val == 1.3);
+    
+    //update it, exiting key w/ new value
+    bool yz = A.update (Key1, 1.3);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (yz == true);
+    assert (A.contains(Key1) == true);
+    assert (A.get(Key1, val) == true && Key1 == "a" && val == 1.3);
+}
+void testUpdate (Map A)
+{
+    // If key is equal to a key currently in the map, then make that key no
+    // longer map to the value it currently maps to, but instead map to
+    // the value of the second parameter; return true in this case.
+    // Otherwise, make no change to the map and return false.
+    
+    KeyType Key1 = "a";
+    KeyType Key2 = "b";
+    KeyType Key3 = "a";
+    
+    ValueType val = 0;
+    //updating an empty map, will not work
+    bool x = A.update(Key1, 1.2);
+    assert (A.size()==0);
+    assert (A.empty()==true);
+    assert (x == false);
+    assert (A.contains(Key1) == false);
+    assert (A.get(Key1, val) == false && Key1 == "a" && val == 0);
+
+    //add an item
+    bool z = A.insert(Key1,1.2);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (z == true);
+    assert (A.contains(Key1) == true);
+    assert (A.get(Key1, val) == true && Key1 == "a" && val == 1.2);
+    
+    //update it, exiting key w/ new value
+    bool y = A.update (Key1, 1.3);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (y == true);
+    assert (A.contains(Key1) == true);
+    assert (A.get(Key1, val) == true && Key1 == "a" && val == 1.3);
+    
+    //update it, exiting key w/ new value
+    bool yz = A.update (Key1, 1.3);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (yz == true);
+    assert (A.contains(Key1) == true);
+    assert (A.get(Key1, val) == true && Key1 == "a" && val == 1.3);
+    cerr << "Update() --> passed" << endl;
+
+}
+
 void testInsert (Map A)
 {
     // If key is not equal to any key currently in the map, and if the
@@ -66,19 +155,32 @@ void testInsert (Map A)
     assert (A.size()==0);
     assert (A.empty()==true);
     
+    //insert new key, new value -> true
     bool x = A.insert (Key1, 1.2);
     assert (A.size()==1);
     assert (A.empty()==false);
     assert (x == true);
+    assert (A.insertOrUpdate(Key1, 1.2) == true);
     assert (A.contains(Key1));
     
+    //insert new key, same value -> false
     ValueType val;
     bool y = A.insert (Key1, 1.2);
     assert (A.size()==1);
     assert (A.empty()==false);
     assert (y == false);
+    assert (A.insertOrUpdate (Key1, 1.2) == true);
     assert (A.get(Key1, val) == true && val == 1.2);
     
+    //insert already key, new value -> false
+    ValueType valz;
+    bool yz = A.insert (Key1, 99.9);
+    assert (A.size()==1);
+    assert (A.empty()==false);
+    assert (yz == false);
+    assert (A.get(Key1, valz) == true && valz == 1.2);
+
+    //insert already key, new value -> false
     ValueType val2 = 1.0;
     bool z = A.insert (Key3, 1.3);
     assert (A.size()==1);
@@ -86,12 +188,13 @@ void testInsert (Map A)
     assert (z == false);
     assert (A.get(Key1, val2) == true && val2 == 1.2);
 
-    
+    //normal, new key w/ new value
     ValueType val3 = 10.1;
     bool za = A.insert (Key2, 1.5);
     assert (A.size()==2);
     assert (A.empty()==false);
     assert (za == true);
+    assert (A.insertOrUpdate (Key2, 1.5) == true);
     assert (A.get(Key2, val3) == true && val3 == 1.5);
     cerr << "Insert() --> passed" << endl;
 
@@ -134,8 +237,6 @@ void test_SizeEmptyContains(Map A)
     //after deleting 1 element
 
 }
-
-
 
 void testErase (Map A)
 {
@@ -205,13 +306,6 @@ void testCombine (Map A, Map B)
     result.dump();
 }
 
-void testUpdate(Map A)
-{
-    bool x = A.update("pigzs", 10.1);
-    cerr << "0: " << x;
-    bool y = A.update("pigzs", 10.1);
-    cerr << "1: " << y;
-}
 void testGet1(Map A)
 {
     A.dump();
