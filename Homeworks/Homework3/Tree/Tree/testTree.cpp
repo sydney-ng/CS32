6b84b9b2257cf0d9c7f7035087e7349f4f67538c
sydney-ng <sydney.ng@yahoo.com>
2018-02-14 17:16:55 -0800

    fixed both fx

---
+++ Homeworks/Homework3/Tree/Tree/testTree.cpp
@@ -0,0 +1,9 @@
+//
+//  testTree.cpp
+//  Tree
+//
+//  Created by Super on 2/14/18.
+//  Copyright © 2018 Sydney. All rights reserved.
+//
+
+#include "testTree.hpp"
---
+++ Homeworks/Homework3/Tree/Tree/testTree.h
@@ -0,0 +1,14 @@
+//
+//  testTree.hpp
+//  Tree
+//
+//  Created by Super on 2/14/18.
+//  Copyright © 2018 Sydney. All rights reserved.
+//
+
+#ifndef testTree_hpp
+#define testTree_hpp
+
+#include <stdio.h>
+int countIncludes(const double a1[], int n1, const double a2[], int n2);
+#endif /* testTree_hpp */
