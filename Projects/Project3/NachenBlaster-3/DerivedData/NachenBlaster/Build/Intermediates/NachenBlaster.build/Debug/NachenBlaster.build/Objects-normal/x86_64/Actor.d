effdb06e9af76aecfe3acc0a39234688bf46a6da
sydney-ng <sydney.ng@yahoo.com>
2018-03-05 21:41:44 -0800

    removed extra folderS

--- Projects/Project3/NachenBlaster-3/DerivedData/NachenBlaster/Build/Intermediates/NachenBlaster.build/Debug/NachenBlaster.build/Objects-normal/x86_64/Actor.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/Actor.cpp \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/Actor.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GraphObject.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GameConstants.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/StudentWorld.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GameWorld.h
--- Projects/Project3/NachenBlaster-3/DerivedData/NachenBlaster/Build/Intermediates/NachenBlaster.build/Debug/NachenBlaster.build/Objects-normal/x86_64/GameWorld.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GameWorld.cpp \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GameWorld.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GameConstants.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/GameController.h \
-  /Users/J/Desktop/CS32/Projects/Project3/NachenBlaster-3/NachenBlaster/SpriteManager.h \
-  /usr/local/Cellar/freeglut/3.0.0/include/GL/freeglut.h \
-  /usr/local/Cellar/freeglut/3.0.0/include/GL/freeglut_std.h \
-  /usr/local/Cellar/freeglut/3.0.0/include/GL/freeglut_ext.h
