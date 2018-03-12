3e8240490400d35d6ea8f860ef4f89b84f58c9c5
sydney-ng <sydney.ng@yahoo.com>
2018-03-11 17:57:55 -0700

    wrote the function for loadwordlist()

--- Projects/Project4/CipherEncrypion/WordList.cpp
+++ Projects/Project4/CipherEncrypion/WordList.cpp
@@ -1,7 +1,11 @@
 #include "provided.h"
+#include "MyHash.h"
 #include <string>
 #include <vector>
 using namespace std;
+//I added these:
+#include <iostream>
+#include <fstream>
 
 class WordListImpl
 {
@@ -10,15 +14,55 @@ public:
     bool contains(string word) const;
     vector<string> findCandidates(string cipherWord, string currTranslation) const;
 private:
+    MyHash <int, string> * m_WLIHash;
+    
 };
 
 bool WordListImpl::loadWordList(string filename)
 {
-    return false;  // This compiles, but may not be correct
+    ifstream infile(filename);    // infile is a name of our choosing
+    if (infile)		        // Did opening the file fail?
+    {
+        cerr << "you opened word list" << endl;
+
+        //create a new hash
+        m_WLIHash = new MyHash<int,string>::MyHash();
+        string readLine;
+        while (getline(infile,readLine))
+        {
+            //TO-DO: MAKE SURE IT'S NOT ALREADY IN THE HASH
+            
+            //input validation
+            bool flag = true;
+            for (int i =0; i < readLine.size(); i++)
+            {
+                //this is a word that is not part of the word list
+                if (isalpha(readLine[i]) == false && readLine[i] != '\'')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            //this is a valid word
+            if (flag == true)
+            {
+                //TO-DO: add it to the Hash
+                unsigned int hash (const string &k);
+                int hashNum = hash(readLine);
+                //TO-DO: change the empty string
+                m_WLIHash->associate(hashNum, readLine);
+            }
+        }
+        return true; //means you were able to open the file
+    }
+    
+    return false;  //unable to open file
 }
 
 bool WordListImpl::contains(string word) const
 {
+    
     return false; // This compiles, but may not be correct
 }
 
@@ -34,7 +78,7 @@ unsigned int hash(const std::string& s)
     return std::hash<std::string>()(s);
 }
 
-/*unsigned int hash(const int& i)
+unsigned int hash(const int& i)
 {
     return std::hash<int>()(i);
 }
@@ -42,7 +86,7 @@ unsigned int hash(const std::string& s)
 unsigned int hash(const char& c)
 {
     return std::hash<char>()(c);
-}*/
+}
 
 //******************** WordList functions ************************************
 
