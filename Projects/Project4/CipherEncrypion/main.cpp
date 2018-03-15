6aaeec825f6e9a678bd16af96e9c63967b983d78
sydney-ng <sydney.ng@yahoo.com>
2018-03-15 00:30:20 -0700

    Hash Copy (not working)
    Otherwise up-to-date for everything else

--- Projects/Project4/CipherEncrypion/main.cpp
+++ Projects/Project4/CipherEncrypion/main.cpp
@@ -17,9 +17,9 @@ using namespace std;
 // CHANGE TO false THE VARIABLES CORRESPONDING TO CLASSES YOU ARE *NOT*
 // YET READY TO TEST
 
-const bool shouldTestMyHash = false;
+const bool shouldTestMyHash = true;
 const bool shouldTestTokenizer = false;
-const bool shouldTestWordList = true;
+const bool shouldTestWordList = false;
 const bool shouldTestTranslator = false;
 const bool shouldTestDecrypter = false;
 
@@ -33,22 +33,51 @@ void testWordList();
 void testTranslator();
 void testDecrypter();
 bool createWordListFile();
+void testTranslatorFromSpec();
+void testTranslatorFromSpec1();
+
 
 int main()
 {
-//    WordList * wlP = new WordList();
-//    wlP->loadWordList("/Users/J/Desktop/CS32/Projects/Project4/CipherEncrypion/wordlist.txt");
-//    cerr << "it has apes is: " << wlP->contains("apes");
-//    
-//    Translator t;
-//    t.pushMapping("abc", "abc");
-//    t.pushMapping("def", "dbf");
-    
+    //testTranslatorFromSpec();
+    //testTranslatorFromSpec1();
     if (shouldTestMyHash    ) { testMyHash();     cout << "Passed MyHash tests" << endl; }
     if (shouldTestTokenizer ) { testTokenizer();  cout << "Passed Tokenizer tests" << endl; }
     if (shouldTestWordList  ) { testWordList();   cout << "Passed WordList tests" << endl; }
     if (shouldTestTranslator) { testTranslator(); cout << "Passed Translator tests" << endl; }
     if (shouldTestDecrypter ) { testDecrypter();  cout << "Passed Decrypter tests" << endl; }
+}
+
+void testTranslatorFromSpec1()
+{
+    Translator t;
+    // Submit the first collection of mappings
+    t.pushMapping("DHL", "ERD"); // DàE, HàR, LàD
+    string secret = "Hdqlx!";
+    cout << t.getTranslation(secret) << endl; // writes Re?d?!
+    // Submit a second collection of mappings
+    t.pushMapping("QX", "AY"); // QàA, XàY
+    cout << t.getTranslation(secret) << endl; // writes Ready!
+    // Pop the most recently pushed collection
+    t.popMapping();
+    cout << t.getTranslation(secret) << endl; // writes Re?d?!
+    // Pop again
+    t.popMapping();
+    cerr << "should write nothing " << endl;
+    cout << t.getTranslation(secret) << endl; // writes ?????!
+}
+
+void testTranslatorFromSpec()
+{
+    Translator t;
+    t.pushMapping("DHL", "ERD"); // DàE, HàR, LàD
+    if ( ! t.pushMapping("QX", "RY")) // QàR, XàY
+        cout << "Both H and Q would map to R!" << endl;
+    // The current mapping is still DàE, HàR, LàD with no
+    // mapping for Q or X
+    cout << t.getTranslation("HDX") << endl; // writes RE?
+    if ( ! t.pushMapping("H", "S")) // HàS
+        cout << "H would map to both R and S!" << endl;
 }
 
 void testMyHash()
@@ -57,19 +86,14 @@ void testMyHash()
     m.associate(222, "Ethel");
     m.associate(333, "Lucy");
     m.associate(111, "Fred");
+    //int xa = m.getNumItems();
     assert(m.getNumItems() == 3);
     assert(abs(m.getLoadFactor() - 0.03) < 0.00001);
-    cerr << "finished here" << endl;
     string* s = m.find(333);
     assert(s != nullptr  &&  *s == "Lucy");
-    cerr << "finished all cases here" << endl;
-
     m.associate(333, "Ricky");
-
     assert(m.getNumItems() == 3);
-
     const MyHash<int, string>& cm = m;
-
     const string* cs = cm.find(333);
     assert(cs != nullptr  &&  *cs == "Ricky");
     int x = 1776;
@@ -78,7 +102,6 @@ void testMyHash()
         m.associate(x, "Carey");
         x += 1789;
     }
-    int xa = m.getNumItems();
     //assert(m.getNumItems() == 1003);
     double lf = m.getLoadFactor();
     assert(lf >= 0.5/2  &&  lf <= 0.5);
@@ -86,7 +109,6 @@ void testMyHash()
     m.associate(444, "David");
     assert(m.getNumItems() == 1);
     assert(abs(m.getLoadFactor() - 0.01) < 0.00001);
-
 }
 
 void testTokenizer()
@@ -99,35 +121,33 @@ void testTokenizer()
 
 void testWordList()
 {
-//    if ( ! createWordListFile())
-//    {
-//        cout << "Cannot complete WordList test" << endl;
-//        exit(1);
-//    }
+    if ( ! createWordListFile())
+    {
+        cout << "Cannot complete WordList test" << endl;
+        exit(1);
+    }
     WordList wl;
-    assert(wl.loadWordList("/Users/J/Desktop/CS32/Projects/Project4/CipherEncrypion/wordlist.txt"));
-    assert(wl.contains("animals"));
-    assert(wl.contains("zoo"));
+    assert(wl.loadWordList(FILENAME));
     assert(wl.contains("expel"));
-    assert(wl.contains("excel"));
     assert(!wl.contains("extol"));
     vector<string> v = wl.findCandidates("wpxWv", "?x???");
     const char* expected[] = { "excel", "expel" };
     assert(v.size() == 2);
-//    for (auto& w : v)
-//        transform(w.begin(), w.end(), w.begin(), [](char c) { return tolower(c); });
-//    assert(equal(v.begin(), v.end(), expected) || equal(v.rbegin(), v.rend(), expected));
-//    v = wl.findCandidates("wpxwv", "?????");
-//    assert(v.size() == 4);
-//    v = wl.findCandidates("wpxwv", "?s??y");
-//    assert(v.size() == 0);
-//    remove(FILENAME);
+    for (auto& w : v)
+        transform(w.begin(), w.end(), w.begin(), [](char c) { return tolower(c); });
+    assert(equal(v.begin(), v.end(), expected) || equal(v.rbegin(), v.rend(), expected));
+    v = wl.findCandidates("wpxwv", "?????");
+    assert(v.size() == 4);
+    v = wl.findCandidates("wpxwv", "?s??y");
+    assert(v.size() == 0);
+    remove(FILENAME);
 }
 
 void testTranslator()
 {
     const char* q5bang = "?????" "!";  // Avoid trigraph
     Translator t;
+    string s = t.getTranslation("Hdqlx!");
     assert(t.getTranslation("Hdqlx!") == q5bang);
     assert( ! t.popMapping());
     assert(t.getTranslation("Hdqlx!") == q5bang);
